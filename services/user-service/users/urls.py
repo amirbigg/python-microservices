f3a2ab12fd793e791210543a6363c8f52308e7d9
amir <amirbig>
2026-09-19 17:20:04 +0330

    Add jwt

--- services/user-service/users/urls.py
+++ services/user-service/users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 from . import views
 
 
 urlpatterns = [
     path("health/", views.HealthCheckView.as_view()),
+    path("register/", views.RegisterView.as_view()),
+    path("login/", TokenObtainPairView.as_view()),
+    path("me/", views.MeView.as_view()),
 ]
